c66dc54b9da2815c62807fcc636a4ad98abd1f79
Farouk <faiz@eurecom.fr>
2022-01-27 18:33:27 +0100

    Discord notif

--- app.js
+++ app.js
@@ -3,34 +3,58 @@ import fetch from 'node-fetch';
 
 
 const asyncFilter = async (arr, predicate) => {
-	const results = await Promise.all(arr.map(predicate));
-	return arr.filter((_v, index) => results[index]);
+    const results = await Promise.all(arr.map(predicate));
+    return arr.filter((_v, index) => results[index]);
 }
 
 const fetchData = async () => {
-    const URL = "https://trouverunlogement.lescrous.fr/tools/flow/21/search?bounds=2.1601_48.9661_2.5391_48.6926&page=1&price=60000"
+    const URL = "https://trouverunlogement.lescrous.fr/tools/flow/21/search?bounds=2.1601_48.9661_2.5391_48.6926"
     // const URL = "https://trouverunlogement.lescrous.fr/tools/flow/21/search?bounds=4.3781_46.2596_6.2622_42.8317"
     const browser = await puppeteer.launch()
     setInterval(
         async () => {
             const page = await browser.newPage();
-            await page.goto(URL)//, { waitUntil: "networkidle2" });
+            await page.goto(URL)
             await page.waitForSelector(".SearchResults-item");
             const results = await page.$$eval(".SearchResults-item", (results) => {
                 return results.map(result => {
                     const id = result.querySelector("[id^='accommodations-']").id.split("-")[1]
                     const name = result.querySelector(".Frame-subtitle").innerHTML
-                    return {id, name}
+                    return { id, name }
                 })
             })
-            const availabilities = await asyncFilter(results, async result => {
+            const residences = await asyncFilter(results, async result => {
                 const response = await fetch(`https://trouverunlogement.lescrous.fr/api/fr/tools/21/accommodations/${result.id}/availabilities?occupationMode=alone&arrivalDate=2022-01-29&departureDate=2022-08-31`)
                 const data = await response.json()
+
                 return data.periodsAvailable.length != 0
             })
-            console.log(availabilities)
-        }, 1000 * 10
+            console.log(residences)
+            if (residences.length){
+                const residencesListMessage = residences.map(residence => `  - ${residence.name}`).join("\n")
+                notifyDiscord(`Hey, I found ${residences.length} available residences!\n${residencesListMessage}`)
+            }
+        }, 1000 * 60 * 5
     )
+}
+
+const notifyDiscord = async (message) => {
+    const DISCORD_URL = 'https://discord.com/api/webhooks/936304587244204073/MDHxQYyzB5UqsFYkk8AfEfSFhXc19oXTk1BaN7LKLv7_ZZl8BDGgWousKN_SUjaXgRx3';
+    var payload = JSON.stringify({
+        username: "Fake estate agent",
+        avatar_url: "https://p7.hiclipart.com/preview/866/59/617/estate-agent-real-estate-computer-icons-house-property-house.jpg",
+        content: message
+    });
+    var params = {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        method: 'POST',
+        body: payload,
+        muteHttpExceptions: true,
+    };
+
+    await fetch(DISCORD_URL, params);
 }
 
 fetchData()
